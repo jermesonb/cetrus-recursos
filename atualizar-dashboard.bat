eec86f1fabab27ffc10572c63a1ecc2dd1e539e9
Jermeson ADM <jermeson.adm@cetrus.com.br>
2026-09-09 20:31:09 -0300

    atualizacao dash 09/09/2026 20:31

--- atualizar-dashboard.bat
+++ atualizar-dashboard.bat
@@ -10,4 +10,5 @@ echo.
 "C:\Program Files\Git\bin\bash.exe" -c "cd '/c/Users/%USERNAME%/Documents/Project/Agendamento de Recursos Extras' && echo 'Pasta encontrada!' && echo && echo '[1/4] Verificando alteracoes...' && git status --short && echo && git add . && git diff --cached --quiet && echo 'Nenhuma alteracao nova para enviar.' || (echo '[2/4] Criando commit...' && git commit -m \"atualizacao dash $(date '+%%d/%%m/%%Y %%H:%%M')\") && echo && echo '[3/4] Atualizando repositorio...' && git pull --rebase && echo && echo '[4/4] Enviando para GitHub...' && git push -u origin main && echo && echo '========================================' && echo '   DASHBOARD ATUALIZADO COM SUCESSO!' && echo '========================================'"
 
 echo.
+echo exit.
 pause
